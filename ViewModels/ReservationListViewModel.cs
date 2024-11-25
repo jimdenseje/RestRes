9c410d54559ccbf69de359f3871d3eae93b85ff7
Jim Haaland Damgaard <jimdenseje@gmail.com>
2024-11-25 13:29:23 +0100

    Aflevering af den modificerede MongoDB Database Opgave

--- ViewModels/ReservationListViewModel.cs
+++ ViewModels/ReservationListViewModel.cs
@@ -5,5 +5,6 @@ namespace RestRes.ViewModels
     public class ReservationListViewModel
     {
         public IEnumerable<Reservation>? Reservations { get; set; }
+        public List<string> Restaurants = new List<string> { };
     }
 }
